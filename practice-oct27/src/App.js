7b95f3e683ccf4f7ad6bf3c878146bc34c8389dc
roselandroche <r.landroche@gmail.com>
2019-10-27 10:31:41 -0400

    Created dumbComponent

--- practice-oct27/src/App.js
+++ practice-oct27/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Simple from './components/dumbComponent';
 
 function App() {
   return (
     <div className="App">
       <div>Practice</div>
+      <Simple />
     </div>
   );
 }
---
+++ practice-oct27/src/components/dumbComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Simple() {
+    return (
+        <>
+            <h1>Title</h1>
+            <h2>Subtitle</h2>
+        </>
+    )
+}
+
+export default Simple;
